11dec2dd7ae882f0eae132cf9e996b2a06a6f822
imontalvomiguel <ubaldo.im@gmail.com>
2017-01-15 15:45:22 -0600

    copy theme files into dist dir

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,7 @@ var gulp = require('gulp');
 var concat = require('gulp-concat');
 var rename = require('gulp-rename');
 var sass = require('gulp-sass');
+var merge = require('merge-stream');
 
 gulp.task('scripts', function() {
   return gulp.src('theme/assets/js/script-*.js')
@@ -19,6 +20,17 @@ gulp.task('sass', function() {
     .pipe(gulp.dest('.build/assets'));
 });
 
-gulp.task('build', ['scripts', 'sass']);
+gulp.task('build', ['scripts', 'sass'], function() {
+  var fonts = gulp.src('theme/assets/static/fonts/**/*')
+    .pipe(gulp.dest('.build/assets'));
+
+  var images = gulp.src('theme/assets/static/img/**/*')
+    .pipe(gulp.dest('.build/assets'));
+
+  var theme = gulp.src(['theme/**/*', '!theme/assets/**/*'])
+    .pipe(gulp.dest('.build'));
+
+  return merge(fonts, images, theme);
+});
 
 gulp.task('default', ['build']);
