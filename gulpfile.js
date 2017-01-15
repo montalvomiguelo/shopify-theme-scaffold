ca60ecd2c85ce0be193eace39c165c8056a809a6
imontalvomiguel <ubaldo.im@gmail.com>
2017-01-15 01:51:59 -0600

    compile sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,20 @@
 'use strict';
 
-var gulp = require('gulp'),
-    concat = require('gulp-concat'),
-    uglify = require('gulp-uglify'),
-    pump = require('pump'),
-    rename = require('gulp-rename');
+var gulp = require('gulp');
+var concat = require('gulp-concat');
+var rename = require('gulp-rename');
+var sass = require('gulp-sass');
 
 gulp.task('scripts', function() {
   return gulp.src('theme/assets/js/script-*.js')
     .pipe(concat('script.js'))
+    .pipe(rename({extname: '.js.liquid'}))
     .pipe(gulp.dest('.build/assets'));
 });
 
-gulp.task('compress', function(cb) {
-  pump([
-    gulp.src('.build/assets/script.js'),
-    uglify(),
-    rename('script.js.liquid'),
-    gulp.dest('.build/assets')
-  ],
-    cb
-  );
+gulp.task('sass', function() {
+  return gulp.src('theme/assets/scss/styles.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(rename({extname: '.css.liquid'}))
+    .pipe(gulp.dest('.build/assets'));
 });
