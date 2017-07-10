324f9eef2eeda0a3b8f080c0ac9235c3e9239c99
imontalvomiguel <ubaldo.im@gmail.com>
2017-07-09 22:43:00 -0500

    add svg icons instead iconfonts

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,37 @@ var del = require('del');
 var runSequence = require('run-sequence');
 var browserSync = require('browser-sync').create();
 
+function processSvg($, file) {
+  var $svg = $('svg');
+  var $newSvg = $('<svg aria-hidden="true" focusable="false" role="presentation" class="icon" />');
+  var fileName = file.relative.replace('.svg', '');
+  var viewBoxAttr = $svg.attr('viewbox');
+
+  // Add necessary attributes
+  if (viewBoxAttr) {
+    var width = parseInt(viewBoxAttr.split(' ')[2], 10);
+    var height = parseInt(viewBoxAttr.split(' ')[3], 10);
+    var widthToHeightRatio = width / height;
+    if (widthToHeightRatio >= 1.5) {
+      $newSvg.addClass('icon--wide');
+    }
+    $newSvg.attr('viewBox', viewBoxAttr);
+  }
+
+  // Add required classes to full color icons
+  if (file.relative.indexOf('-full-color') >= 0) {
+    $newSvg.addClass('icon--full-color');
+  }
+
+  $newSvg
+    .addClass(fileName)
+    .append($svg.contents());
+
+  $newSvg.append($svg.contents());
+  $svg.after($newSvg);
+  $svg.remove();
+}
+
 gulp.task('scripts', function() {
   return gulp.src(['theme/assets/js/script-*.js'])
     .pipe($.newer('.build/assets/script.js.liquid'))
@@ -28,22 +59,34 @@ gulp.task('sass', function() {
 });
 
 gulp.task('copy', function() {
-  var fonts = gulp.src('theme/assets/static/fonts/**/*')
-    .pipe($.newer('.build/assets'))
-    .pipe(gulp.dest('.build/assets'));
-
   var images = gulp.src('theme/assets/static/img/**/*')
     .pipe($.newer('.build/assets'))
     .pipe(gulp.dest('.build/assets'));
 
-  var theme = gulp.src(['theme/**/*', '!theme/assets/**/*'])
+  var theme = gulp.src(['theme/**/*', '!theme/assets/**/*', '!theme/icons/**/*'])
     .pipe($.newer('.build'))
     .pipe(gulp.dest('.build'));
 
-  return merge(fonts, images, theme);
+  return merge(images, theme);
 });
 
-gulp.task('serve', ['scripts', 'sass', 'copy'], function() {
+gulp.task('icons', function() {
+  return gulp.src('theme/icons/**/*.svg')
+    .pipe($.newer({
+      dest: '.build/snippets',
+      ext: '.liquid'
+    }))
+    .pipe($.svgmin({
+      plugins: [{removeTitle: true}, {removeDesc: true}],
+    }))
+    .pipe($.cheerio({
+      run: processSvg
+    }))
+    .pipe($.rename({extname: '.liquid'}))
+    .pipe(gulp.dest('.build/snippets'));
+});
+
+gulp.task('serve', ['scripts', 'sass', 'copy', 'icons'], function() {
   browserSync.init({
     proxy: 'https://your-store.myshopify.com',
     injectChanges: false,
@@ -54,9 +97,10 @@ gulp.task('serve', ['scripts', 'sass', 'copy'], function() {
   gulp.watch([
     'theme/**/*',
     '!theme/assets/**/*',
+    '!theme/icons/**/*',
     'theme/assets/static/img/**/*',
-    'theme/assets/static/fonts/**/*'
   ], ['copy']);
+  gulp.watch('theme/icons/**/*.svg', ['icons']);
 
   gulp.watch('/tmp/theme.update').on('change', browserSync.reload);
 });
@@ -96,9 +140,9 @@ gulp.task('compress', function() {
 gulp.task('default', ['serve']);
 
 gulp.task('build', function() {
-  runSequence(['scripts', 'copy'], 'minify');
+  runSequence(['scripts', 'copy', 'icons'], 'minify');
 });
 
 gulp.task('dist', function() {
-  runSequence('clean', ['scripts', 'copy'], 'minify', 'compress');
+  runSequence('clean', ['scripts', 'copy', 'icons'], 'minify', 'compress');
 });
